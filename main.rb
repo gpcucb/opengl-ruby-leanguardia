0980a5e352f6ba496d9235234c230acc3ca2cab7
redes <lean.guardia93@gmail.com>
2017-04-25 20:30:29 -0400

    triangulo intersectado

--- main.rb
+++ main.rb
@@ -7,32 +7,41 @@ include Glu
 include Glut
 
 def disp
+	glDepthFunc(GL_LEQUAL);
+	glEnable(GL_DEPTH_TEST);
+	glClearDepth(1.0)
+
 	glClearColor(0,0,0,0) 		# background Color
-	glClear(GL_COLOR_BUFFER_BIT) # borramos la pantalla
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # borramos la pantalla
+
 	glMatrixMode(GL_PROJECTION) # Modo proyección
+	#glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
 	glLoadIdentity
-	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
+	gluPerspective(60.0,1.0,1.0,100.0)
 
 	glMatrixMode(GL_MODELVIEW)
+	glTranslatef(0.0,0.0,-2.0)
+
 	glBegin(GL_QUADS)
-		glColor3f(1,0,0)
-		glVertex(-0.6,0.8,0)
-
-		glColor3f(0,1,0)
-		glVertex(0.6,0.8,0)
-
 		glColor3f(1,0,1)
-		glVertex(0.6,-0.2,0)
+		glVertex(-0.5,0.5,-0.5)
+		glVertex(-0.5,-0.5,0.5)
+		glVertex(0.5,-0.5,0.5)
+		glVertex(0.5,0.5,-0.5)
+	glEnd
 
-		glColor3f(0,1,1)
-		glVertex(-0.6,-0.2,0)
+	glBegin(GL_TRIANGLES)
+		glColor3f(1,0.5,1)
+		glVertex(0,0.5,0)
+		glVertex(-0.7,-0.5,0)	
+		glVertex(0.7,-0.5,0)
 	glEnd
 	glFlush
-	glutPostRedisplay
+	#glutPostRedisplay
 end
 
 glutInit
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH)
 glutInitWindowSize(500, 500)
 glutInitWindowPosition(20, 20)
 glutCreateWindow("Hola OpenGL, en Ruby")
